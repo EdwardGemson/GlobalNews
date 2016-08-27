ab1f6b99f0e94beb5bee11591de4b7d069c2e73a
Edward Gemson <edwardgemson@Edwards-MacBook-Pro.local>
2016-08-27 12:00:19 -0400

    complete article migration

--- db/migrate/20160827155623_create_articles.rb
+++ db/migrate/20160827155623_create_articles.rb
@@ -1,6 +1,13 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
+    	t.string :title, null: false
+    	t.integer :coordinates, null: false
+    	t.string :location, null: false
+    	t.string :url, null: false
+    	t.string :topic
+    	t.string :date
+    	t.timestamps null: false
     end
   end
 end
